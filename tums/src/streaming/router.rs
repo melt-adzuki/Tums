20066fefebfdb85dcb14b5efa163ccebd29445ff
melt_adzuki <rebanira02@gmail.com>
2023-02-19 18:04:08 +0900

    feat: router is now fully working!

--- tums/src/streaming/router.rs
+++ tums/src/streaming/router.rs
@@ -1,4 +1,5 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use regex::Regex;
 
 use crate::init::SERVICE;
 
@@ -14,11 +15,44 @@ pub(crate) async fn route(streaming_body: StreamingBody) -> Result<()> {
             let content = note_body.text.unwrap_or_default();
             SERVICE.add_uni_from_dust(content, note_body.id).await?;
         }
-        ChannelType::Mention => {
-            todo!()
-        }
-        ChannelType::Reply => {
-            todo!()
+        ChannelType::Mention | ChannelType::Reply => {
+            let context = note_body.text.unwrap_or_default();
+            let context = context
+                .split_whitespace()
+                .filter(|s| {
+                    Regex::new(r"^@(?:[a-zA-Z]|\d|_)+(?:@([a-zA-Z]|\d|-)+\.[a-z]+)?$")
+                        .is_ok_and(|r| !r.is_match(s))
+                })
+                .collect::<Vec<_>>();
+
+            let command = *context
+                .first()
+                .context("The command of the context cannot be found.")?;
+
+            match command {
+                "show" => SERVICE.show_uni().await?,
+                "list" => SERVICE.list_uni().await?,
+                "add" => {
+                    let pos: i32 = context
+                        .get(1)
+                        .context("The position command cannot be found.")?
+                        .parse()?;
+                    let content = context
+                        .get(2..)
+                        .context("The context on the command cannot be found.")?
+                        .join(" ");
+                    SERVICE.add_uni(content, pos).await?;
+                }
+                "remove" => {
+                    let pos: i32 = context
+                        .get(1)
+                        .context("The position command cannot be found.")?
+                        .parse()?;
+                    let reply_id = note_body.id;
+                    SERVICE.remove_uni(pos, reply_id).await?;
+                }
+                _ => {}
+            };
         }
         _ => {}
     };
